36eb67a40282b833ba91c372d4099dac64268699
Matias-Churquina <matiasdanielchurquina@gmail.com>
2026-04-27 16:41:01 -0300

    Se implementa bootstrap y responsive a la pagina Agencias.

--- js/agencias.js
+++ js/agencias.js
@@ -1,18 +1,24 @@
-document.querySelectorAll(".rating").forEach(rating => {
-    // número aleatorio de 1 a 5
-    const valor = Math.floor(Math.random() * 5) + 1;
+$(document).ready(function () {
 
-    rating.setAttribute("data-rating", valor);
+    // FLIP
+    $(".card-agencia").click(function () {
+        $(this).toggleClass("flip");
+    });
 
-    let estrellas = "";
+    // RATING
+    $(".rating").each(function () {
+        let valor = Math.floor(Math.random() * 5) + 1;
+        let estrellas = "";
 
-    for (let i = 1; i <= 5; i++) {
-        if (i <= valor) {
-            estrellas += "<span>★</span>";
-        } else {
-            estrellas += "☆";
+        for (let i = 1; i <= 5; i++) {
+            if (i <= valor) {
+                estrellas += `<span class="star active" style="--delay:${i * 0.2}s">★</span>`;
+            } else {
+                estrellas += `<span class="star" style="--delay:${i * 0.2}s">★</span>`;
+            }
         }
-    }
 
-    rating.innerHTML = estrellas;
+        $(this).html(estrellas);
+    });
+
 });
